20d01fdc6624671e2bf25ec535f25fd634fd5315
hailiting <hailiting@yeah.net>
2022-09-08 16:00:29 +0800

    KittyIndex 不在 pallet 中指定，而是在 runtime 里绑定

--- v2_class02/pallets/kitties/src/lib.rs
+++ v2_class02/pallets/kitties/src/lib.rs
@@ -15,21 +15,46 @@ pub mod pallet {
   use frame_system::pallet_prelude::*;
   use frame_support::traits::Randomness;
   use sp_io::hashing::blake2_128;
+  use sp_runtime::traits::{
+    Member,
+    MaybeSerializeDeserialize,
+    AtLeast32BitUnsigned, 
+    MaybeDisplay,
+    MaybeMallocSizeOf,
+    Bounded
+  };
+  use sp_std::{fmt::Debug};
+// type KittyIndex = u32;
 
-  type KittyIndex = u32;
-
-  #[pallet::type_value]
-  pub fn GetDefaultValue() -> KittyIndex {
-    0_u32
-  }
 
   #[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
   pub struct Kitty(pub [u8; 16]);
 
   #[pallet::config]
   pub trait Config: frame_system::Config {
-    type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-    type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
+      type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+      type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
+      type KittyIndex: Parameter
+			+ Member
+			+ MaybeSerializeDeserialize
+			+ Debug
+			+ MaybeDisplay
+			+ AtLeast32BitUnsigned
+			+ Default
+			+ Bounded
+			+ Copy
+			+ sp_std::hash::Hash
+			+ sp_std::str::FromStr
+			+ MaybeMallocSizeOf
+			+ MaxEncodedLen
+			+ TypeInfo;
+
+  }
+
+  
+  #[pallet::type_value]
+  pub fn GetDefaultValue<T: Config>() -> T::KittyIndex {
+    0u32.into()
   }
 
   #[pallet::pallet]
@@ -38,31 +63,32 @@ pub mod pallet {
 
 	#[pallet::storage]
 	#[pallet::getter(fn next_kitty_id)]
-	pub type NextKittyId<T> = StorageValue<_, KittyIndex, ValueQuery, GetDefaultValue>;
+	pub type NextKittyId<T: Config> = StorageValue<_, T::KittyIndex, ValueQuery, GetDefaultValue<T>>;
 
 
 
   #[pallet::storage]
   #[pallet::getter(fn kitties)]
-  pub type Kitties<T> = StorageMap<_, Blake2_128Concat, KittyIndex, Kitty>;
+  pub type Kitties<T: Config> = StorageMap<_, Blake2_128Concat, T::KittyIndex, Kitty>;
 
   #[pallet::storage]
   #[pallet::getter(fn kitty_owner)]
-  pub type KittyOwner<T: Config> = StorageMap<_, Blake2_128Concat, KittyIndex, T::AccountId>;
+  pub type KittyOwner<T: Config> = StorageMap<_, Blake2_128Concat, T::KittyIndex, T::AccountId>;
 
   #[pallet::event]
   #[pallet::generate_deposit(pub(super) fn deposit_event)]
   pub enum Event<T:Config> {
-    KittyCreated(T::AccountId, KittyIndex, Kitty),
-    KittyBreed(T::AccountId, KittyIndex, Kitty),
-    KittyTransfered(T::AccountId, T::AccountId, KittyIndex)
+    KittyCreated(T::AccountId, T::KittyIndex, Kitty),
+    KittyBreed(T::AccountId, T::KittyIndex, Kitty),
+    KittyTransfered(T::AccountId, T::AccountId, T::KittyIndex)
   }
 
   #[pallet::error]
   pub enum Error<T>{
-    InvalidKittyId,
+    KittiesCountOverflow,
     NotOwner,
-    SameKittyId
+    SameKittyId,
+    InvalidKittyId,
   }
   #[pallet::call]
   impl<T:Config> Pallet<T> {
@@ -72,15 +98,11 @@ pub mod pallet {
       let kitty_id = Self::get_next_id().map_err(|_| Error::<T>::InvalidKittyId)?;
       let dna = Self::random_value(&who);
       let kitty = Kitty(dna);
-
-      Kitties::<T>::insert(kitty_id, &kitty);
-      KittyOwner::<T>::insert(kitty_id, &who);
-      NextKittyId::<T>::set(kitty_id+1);
-      Self::deposit_event(Event::KittyCreated(who, kitty_id, kitty));
+      Self::created(who, kitty_id, kitty);
       Ok(())
     }
     #[pallet::weight(10_000)]
-    pub fn breed(origin:OriginFor<T>, kitty_id_1: KittyIndex, kitty_id_2:KittyIndex)->DispatchResult{
+    pub fn breed(origin:OriginFor<T>, kitty_id_1: T::KittyIndex, kitty_id_2:T::KittyIndex)->DispatchResult{
       let who = ensure_signed(origin)?;
       ensure!(kitty_id_1!=kitty_id_2, Error::<T>::SameKittyId);
       let kitty_1 = Self::get_kitty(kitty_id_1).map_err(|_| Error::<T>::InvalidKittyId)?;
@@ -94,15 +116,12 @@ pub mod pallet {
         data[i] = (kitty_1.0[i] & selector[i]) | (kitty_2.0[i]&!selector[i]);
       }
       let new_kitty = Kitty(data);
-      Kitties::<T>::insert(kitty_id, &new_kitty);
-      KittyOwner::<T>::insert(kitty_id, &who);
-      NextKittyId::<T>::set(kitty_id+1);
-
-      Self::deposit_event(Event::KittyBreed(who, kitty_id, new_kitty));
+      Self::deposit_event(Event::KittyBreed(who.clone(), kitty_id, new_kitty.clone()));
+      Self::created(who, kitty_id, new_kitty);
       Ok(())
     }
     #[pallet::weight(10_000)]
-    pub fn transfer(origin:OriginFor<T>,kitty_id:u32, new_owner:T::AccountId)->DispatchResult {
+    pub fn transfer(origin:OriginFor<T>,kitty_id: T::KittyIndex, new_owner:T::AccountId)->DispatchResult {
       let who = ensure_signed(origin)?;
       Self::get_kitty(kitty_id).map_err(|_| Error::<T>::InvalidKittyId)?;
       ensure!(Self::kitty_owner(kitty_id)==Some(who.clone()), Error::<T>::NotOwner);
@@ -112,7 +131,7 @@ pub mod pallet {
     }
   }
 
-  impl<T:Config> Pallet<T>{
+  impl<T: Config> Pallet<T>{
     fn random_value(sender: &T::AccountId)->[u8; 16] {
       let payload = (
         T::Randomness::random_seed(),
@@ -121,17 +140,26 @@ pub mod pallet {
       );
       payload.using_encoded(blake2_128)
     }
-    fn get_next_id()->Result<KittyIndex, ()> {
-      match Self::next_kitty_id(){
-        KittyIndex::MAX=>Err(()),
-        val=>Ok(val)
+    fn get_next_id() -> sp_std::result::Result<T::KittyIndex, DispatchError> {
+      let _max_value_local = T::KittyIndex::max_value();
+      let next_kitty_id = Self::next_kitty_id();
+      if next_kitty_id == _max_value_local {
+        return Err(Error::<T>::KittiesCountOverflow.into());
       }
+      Ok(next_kitty_id)
     }
-    fn get_kitty(kitty_id:KittyIndex)->Result<Kitty,()>{
+
+    fn get_kitty(kitty_id: T::KittyIndex)->Result<Kitty,()>{
       match Self::kitties(kitty_id){
         Some(kitty)=>Ok(kitty),
         None=>Err(())
       }
+    }
+    fn created(who: T::AccountId, kitty_id: T::KittyIndex, kitty: Kitty){
+      Kitties::<T>::insert(kitty_id, kitty.clone());
+      KittyOwner::<T>::insert(kitty_id, who.clone());
+      NextKittyId::<T>::set(kitty_id+1u32.into());
+      Self::deposit_event(Event::KittyCreated(who, kitty_id, kitty));
     }
   }
 }
--- v2_class02/pallets/kitties/src/mock.rs
+++ v2_class02/pallets/kitties/src/mock.rs
@@ -51,6 +51,8 @@ impl system::Config for Test {
 
 impl pallet_kitties::Config for Test {
 	type Event = Event;
+	type Randomness = RandomnessCollectiveFlip;
+	type KittyIndex = u32;
 }
 
 // Build genesis storage according to the mock runtime.
